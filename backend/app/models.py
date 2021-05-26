15e0c33f4588719b2d4e9495596946cc56da0045
songer <weixiao.song@outlook.com>
2021-05-26 16:11:08 +0800

    update models

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,6 +4,91 @@ from datetime import time
 from . import db
 from sqlalchemy.sql import column
 
+class Template(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(40))
+    description = db.Column(db.String(200))
+    approved = db.Column(db.Boolean)
+
+    infos = db.relationship('Info', backref='template', lazy='dynamic')
+
+    def __init__(self, title, description, company_id):
+        self.title = title
+        self.description = description
+        self.company_id = company_id
+        self.approved = False
+
+
+class Info(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    create_time = db.Column(db.String(20))
+    modify_time = db.Column(db.String(20))
+    content = db.Column(db.String(100))
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    template_id = db.Column(db.Integer, db.ForeignKey('template.id'))
+
+    def __init__(self, content, tempalte_id, user_id):
+        self.content = content
+        self.template_id = tempalte_id
+        self.user_id = user_id
+        self.create_time = time.asctime( time.localtime( time.time()))
+        self.modify_time = time.asctime( time.localtime( time.time()))
+
+
+infos = db.Table('infos',
+    db.Column('account_id', db.Integer, db.ForeignKey('account.id')),
+    db.Column('info_id', db.Integer, db.ForeignKey('info.id')),
+)
+
+class Account(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    infos = db.relationship('Info', secondary=infos,
+        backref = db.backref('accounts', lazy='dynamic'), lazy='dynamic')
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'))
+
+    def __init__(self, user_id, company_id):
+        self.user_id = user_id,
+        self.company_id = company_id
+
+    def __repr__(self):
+        return '<User-Company %r-%r>' % self.user
+
+
+class Requirement(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    # 权限类型 (read, all)
+    permission = db.Column(db.Integer)
+    
+    template_id = db.Column(db.Integer, db.ForeignKey('template.id'))
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'))
+
+
+class Company(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True)
+    password = db.Column(db.String(32))
+
+    accounts = db.relationship('Account', backref='company', lazy='dynamic')
+    requirements = db.relationship('Requirement', backref='company', lazy='dynamic')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return '<Company %r>' % self.username
+
+
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
@@ -13,7 +98,7 @@ class User(db.Model):
 
     accounts = db.relationship(
         'Account', backref='user', lazy='dynamic')
-    infos = db.relationship('Info', backref='owner', lazy='dynamic')
+    infos = db.relationship('Info', backref='user', lazy='dynamic')
 
     def __init__(self, username, password, email = 'test@test.com'):
         self.username = username
@@ -31,105 +116,7 @@ class User(db.Model):
         }
 
 
-class Company(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True)
-    password = db.Column(db.String(32))
-
-    accounts = db.relationship('Account', backref='company', lazy='dynamic')
-    templates = db.relationship('Template', backref='owner', lazy='dynamic')
-
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
-    def __repr__(self):
-        return '<Company %r>' % self.username
-
-
 class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True)
     password = db.Column(db.String(32))
-
-
-class Template(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(40))
-    description = db.Column(db.String(200))
-    approved = db.Column(db.Boolean)
-
-    infos = db.relationship('Info', backref='template', lazy='dynamic')
-
-    company_id = db.Column(db.Integer, db.ForeignKey('company.id'))
-
-    def __init__(self, title, description, company_id):
-        self.title = title
-        self.description = description
-        self.company_id = company_id
-        self.approved = False
-
-
-class Info(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    create_time = db.Column(db.String(20))
-    modify_time = db.Column(db.String(20))
-    content = db.Column(db.String(100))
-
-    wrappers = db.relationship(
-        'InfoWrapper', backref='info_content', lazy='dynamic')
-
-
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    template_id = db.Column(db.Integer, db.ForeignKey('template.id'))
-
-    def __init__(self, content, tempalte_id, user_id):
-        self.content = content
-        self.template_id = tempalte_id
-        self.user_id = user_id
-        self.create_time = time.asctime( time.localtime( time.time()))
-        self.modify_time = time.asctime( time.localtime( time.time()))
-
-
-class InfoWrapper(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-
-    # 权限类型
-    permission = db.Column(db.Integer)
-    
-    info_id = db.Column(db.Integer, db.ForeignKey('info.id'))
-    user_company_rela_id = db.Column(db.Integer, db.ForeignKey('account.id'))
-
-
-class Account(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-
-    login_histories = db.relationship(
-        'LoginHistory', backref='account', lazy='dynamic')
-
-    info_list = db.relationship(
-        'InfoWrapper', backref='account', lazy='dynamic')
-
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    company_id = db.Column(db.Integer, db.ForeignKey('company.id'))
-
-    def __init__(self, user_id, company_id):
-        self.user_id = user_id,
-        self.company_id = company_id
-
-    def __repr__(self):
-        return '<User-Company %r-%r>' % self.user
-
-
-class LoginHistory(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    ip = db.Column(db.String(30))
-    device = db.Column(db.String(20))
-    
-    account_id = db.Column(db.Integer, db.ForeignKey('account.id'))

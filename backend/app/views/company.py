6b03ab27a6a415411df341a123a2c312060503f1
songer <weixiao.song@outlook.com>
2021-05-28 02:43:25 +0800

    finish company api

--- backend/app/views/company.py
+++ backend/app/views/company.py
@@ -1,7 +1,9 @@
 
+import typing
+from app.status_code import MISSING_ARGUMENT
 from flask import json
 from app.auth.oauth import generate_oauth_key
-from app.auth.jwt import jwt_auth, generate_jwt_token_for_company
+from app.auth.jwt import admin_login_required, jwt_auth, generate_jwt_token_for_company
 from http import HTTPStatus
 from app import models, db
 from app.utils import company_login_required, encode_password
@@ -12,13 +14,26 @@ bp = Blueprint('company', __name__, url_prefix='/company')
 
 @bp.route('/register', methods=['POST'])
 def register():
-    # 邮箱验证？管理员审核？
     data = request.get_json()
     username = data['username']
     password = data['password']
+    description = data['description']
 
-    # TODO
-    pass
+    if (username is None or password is None or description is None):
+        return jsonify(message=MISSING_ARGUMENT), HTTPStatus.BAD_REQUEST
+
+    if (models.Company.query.filter(username=username, status='approved').first() != None):
+        return jsonify(message='企业用户名已被注册'), HTTPStatus.BAD_REQUEST
+    
+    company = models.Company(username, encode_password(password), description)
+
+    db.session.add(company)
+    db.session.commit()
+
+    if (models.Company.query.filter(username=username).first() is None):
+        return jsonify({'message': '注册失败'}), HTTPStatus.BAD_REQUEST
+
+    return jsonify({'message': '注册成功，请前往登录界面登录'}), HTTPStatus.CREATED
 
 @bp.route('/login', methods=['POST'])
 def login():
@@ -26,7 +41,7 @@ def login():
     username = data['username']
     password = data['password']
 
-    company = models.Company.query.filter_by(username=username).first()
+    company = models.Company.query.filter(username=username).first()
 
     if (company is None):
         resp = jsonify({"message":"企业用户不存在"}), HTTPStatus.BAD_REQUEST
@@ -36,16 +51,38 @@ def login():
         resp = jsonify({"message":"密码错误"}), HTTPStatus.BAD_REQUEST
         return resp
 
+    if (company.status == 'waiting'):
+        resp = jsonify(message='正在审批中，请耐心等待'), HTTPStatus.BAD_REQUEST
+        return resp
+
+    if (company.status == 'unapproved'):
+        resp = jsonify(message='审批不通过，请尝试重新申请'), HTTPStatus.BAD_REQUEST
+        return resp
+
     token = generate_jwt_token_for_company(company)
 
     resp = jsonify({'message': '登陆成功', 'token': token})
     return resp, HTTPStatus.OK
 
 
+@bp.route('/get_oauth_key', methods=['GET'])
+@company_login_required
+def get_oauth_key():
+    company = jwt_auth.current_user()
+
+    client_id = company.client_id
+    secret_key = company.secret_key
+
+    return jsonify({
+        'client_id': client_id,
+        'secret_key': secret_key,
+    }), HTTPStatus.OK
+
+
 @bp.route('/regenerate_oauth_key', methods=['POST'])
 @company_login_required
 def regenerate_oauth_key():
-    company = jwt_auth.current_user
+    company = jwt_auth.current_user()
 
     ak, sk = generate_oauth_key(company)
     company.client_id = ak
@@ -53,3 +90,71 @@ def regenerate_oauth_key():
     db.session.commit()
 
     return jsonify(message = '生成成功'), HTTPStatus.OK
+
+
+@bp.route('/modify_password', methods=['POST'])
+@company_login_required
+def modify_password():
+    user : models.Company = jwt_auth.current_user()
+
+    data = request.get_json()
+
+    old_password = data['old_password']
+    new_password = data['new_password']
+
+    if (encode_password(old_password) == user.password):
+        user.password = encode_password(new_password)
+        db.session.commit()
+    else:
+        return jsonify({'message': '旧密码错误'}), HTTPStatus.BAD_REQUEST
+
+
+    return jsonify({'message': '更改密码成功'}), HTTPStatus.OK
+
+
+@bp.route('/approve', methods=['POST'])
+@admin_login_required
+def approve():
+    data = request.get_json()
+    company_id = data.get('company_id', None)
+    status = data.get('status', None)
+
+    if (status not in ['approved', 'unapproved']):
+        return jsonify(message='status参数不合法'), HTTPStatus.BAD_REQUEST
+
+    if (company_id is None or status is None):
+        return jsonify(message=MISSING_ARGUMENT), HTTPStatus.BAD_REQUEST
+
+    company = models.Company.query.get(company_id)
+
+    company.status = status
+
+    db.session.commit()
+
+    return jsonify({"message": "succeed"}), HTTPStatus.OK
+
+
+@bp.route('/get', methods=['GET'])
+@admin_login_required
+def get():
+    
+    data = request.args
+    status = data.get('status', None)
+
+    if (status is None):
+        return jsonify(message=MISSING_ARGUMENT), HTTPStatus.BAD_REQUEST
+    if (status not in ['approved', 'unapproved', 'all']):
+        return jsonify(message='status参数不合法'), HTTPStatus.BAD_REQUEST
+
+    companies : typing.List[models.Company]
+    if (status != 'all'):
+        companies = models.Company.query.filter(status=status).all()
+    else:
+        companies = models.Company.query.all()
+
+    return jsonify({
+        'companies': [
+            company.to_dict()
+            for company in companies
+        ] 
+    }), HTTPStatus.OK

aee18fb65ae5af22c1fd95eb90632138f521e4fc
songer <weixiao.song@outlook.com>
2021-05-29 00:10:51 +0800

    add oauth encode function

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,7 +1,13 @@
 
 import hashlib
+import typing
 
 def encode_password(password):
     h = hashlib.md5(password.encode())
     return h.hexdigest()
 
+def encode_with_nonce_and_timestamp(key_list : typing.List[int]):
+    key_list.sort()
+    s = ''.join(map(str, key_list))
+    h = hashlib.md5(s)
+    return h.hexdigest()

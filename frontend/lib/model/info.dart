b9b9bbc88d3e8b2b5fb1164b12c0ac4a83b238fa
songer <songer@songer.xyz>
2021-06-01 01:24:50 +0800

    update info model

--- frontend/lib/model/info.dart
+++ frontend/lib/model/info.dart
@@ -1,24 +1,101 @@
+import 'dart:convert';
+import 'dart:html';
+
 import 'package:easy_pass/model/backend_client.dart';
+import 'package:easy_pass/utils/function.dart';
 
 class Info {
-  Info({required this.title, required this.content});
+  late int infoId;
+  late String title;
+  late String modifyTime;
+  String? content;
+  String? createTime;
+  int? templateId;
 
-  late int id;
-  String title;
-  String content;
+  Info._fromJson(Map<String, dynamic> dict) {
+    title = dict['title']!;
+    infoId = dict['info_id']!;
+    modifyTime = dict['modify_time']!;
+    createTime = dict['create_time'];
+    content = dict['content'];
+    templateId = dict['template_id'];
+  }
 
-  Info.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        title = json['title'],
-        content = json['content'];
-
-  Map<String, dynamic> toJson() => {
-        'title': title,
-      };
-
-  Future<BackendResponse> register() async {
-    return BackendClient.post('/int/get', {
-      'id': id.toString(),
+  static Future<List<Info>?> get(String keywords, String filterMethod) async {
+    var res = await BackendClient().get("/info/get", {
+      "keywords": keywords,
+      "filter_method": filterMethod,
     });
+    var data = json.decode(res.body);
+
+    if (res.statusCode != HttpStatus.ok) {
+      toast(data['message']);
+      return null;
+    }
+
+    List<Info> infoList = [];
+    for (var info in data['infos']) {
+      infoList.add(Info._fromJson(info));
+    }
+
+    return infoList;
+  }
+
+  static Future<Info?> getDetail(int infoId) async {
+    var res = await BackendClient().get("/info/get_detail", {
+      "info_id": infoId,
+    });
+    var data = json.decode(res.body);
+
+    if (res.statusCode != HttpStatus.ok) {
+      toast(data['message']);
+      return null;
+    }
+
+    var info = Info._fromJson(data['info']);
+    return info;
+  }
+
+  static Future<String?> create(int templateId, String content) async {
+    var res = await BackendClient().post("/info/create", {
+      "template_id": templateId,
+      "content": content,
+    });
+    var data = json.decode(res.body);
+
+    if (res.statusCode != HttpStatus.created) {
+      toast(data['message']);
+      return null;
+    }
+
+    return data['message'];
+  }
+
+  static Future<String?> modify(int infoId, String content) async {
+    var res = await BackendClient().post("/info/modify", {
+      "info_id": infoId,
+      "content": content,
+    });
+    var data = json.decode(res.body);
+
+    if (res.statusCode != HttpStatus.created) {
+      toast(data['message']);
+      return null;
+    }
+
+    return data['message'];
+  }
+
+  static Future<String?> drop(int infoId) async {
+    var res = await BackendClient().post("/info/create", {
+      "info_id": infoId,
+    });
+    var data = json.decode(res.body);
+
+    if (res.statusCode != HttpStatus.created) {
+      toast(data['message']);
+      return null;
+    }
+    return data['message'];
   }
 }

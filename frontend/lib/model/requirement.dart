dc50756bf12f014df07d76207e78a536df0deb14
songer <songer@songer.xyz>
2021-06-01 02:39:52 +0800

    add requirement model

---
+++ frontend/lib/model/requirement.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+import 'dart:html';
+
+import 'package:easy_pass/model/backend_client.dart';
+import 'package:easy_pass/utils/function.dart';
+
+class Requirement {
+  late int requirementId;
+  late int templateId;
+  late int companyId;
+  late String permission;
+  late bool optional;
+
+  static Future<String?> create(
+      int templateId, String permission, bool optional) async {
+    var res = await BackendClient().post("/requirement/create", {
+      "template_id": templateId,
+      "permission": permission,
+      "optional": optional,
+    });
+    var data = json.decode(res.body);
+
+    if (res.statusCode != HttpStatus.created) {
+      toast(data['message']);
+      return null;
+    }
+
+    return data['message'];
+  }
+
+  static Future<String?> modify(
+      int templateId, String permission, bool optional) async {
+    var res = await BackendClient().post("/requirement/modify", {
+      "template_id": templateId,
+      "permission": permission,
+      "optional": optional,
+    });
+    var data = json.decode(res.body);
+
+    if (res.statusCode != HttpStatus.created) {
+      toast(data['message']);
+      return null;
+    }
+
+    return data['message'];
+  }
+
+  static Future<String?> remove(int requirementId) async {
+    var res = await BackendClient().post("/requirement/remove", {
+      "requirement_id": requirementId,
+    });
+    var data = json.decode(res.body);
+
+    if (res.statusCode != HttpStatus.ok) {
+      toast(data['message']);
+      return null;
+    }
+    return data['message'];
+  }
+}

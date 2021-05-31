32e0f120c36bdf87dc77c97205dc2bb0178ae65e
songer <songer@songer.xyz>
2021-06-01 00:50:59 +0800

    add user check

--- frontend/lib/welcome.dart
+++ frontend/lib/welcome.dart
@@ -1,52 +1,71 @@
 import 'package:easy_pass/utils/components.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:styled_widget/styled_widget.dart';
+import 'package:useful_widgets/widgets/future/future_widget.dart';
+
+import 'model/user.dart';
 
 class WelcomePage extends StatelessWidget {
+  Future<bool> checkLogin(BuildContext context) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (prefs.containsKey("userToken")) {
+      bool check = await User.check();
+      if (check == true) {
+        Navigator.of(context)
+            .pushNamedAndRemoveUntil('/home', (route) => false);
+      }
+    }
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(height: 80),
-          Center(
-            child: SvgPicture.asset(
-              'assets/images/logo/easypass-logo-with-text.svg',
+      body: FutureWidget<bool>(
+        future: (context) => checkLogin(context),
+        builder: (context, result) => Column(
+          children: [
+            SizedBox(height: 80),
+            Center(
+              child: SvgPicture.asset(
+                'assets/images/logo/easypass-logo-with-text.svg',
+              ),
             ),
-          ),
-          SizedBox(height: 120),
-          Center(
-            child: Column(
-              children: [
-                MyButton(
-                  child: Text("用户登录"),
-                  onTap: () {
-                    Navigator.of(context).pushNamed('/login');
-                  },
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                MyButton(
-                  child: Text("测试界面"),
-                  onTap: () {
-                    Navigator.of(context).pushNamed('/test');
-                  },
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                MyButton(
-                  child: Text("用户界面"),
-                  onTap: () {
-                    Navigator.of(context).pushNamed('/home');
-                  },
-                ),
-              ],
+            SizedBox(height: 120),
+            Center(
+              child: Column(
+                children: [
+                  MyButton(
+                    child: Text("用户登录"),
+                    onTap: () {
+                      Navigator.of(context).pushNamed('/login');
+                    },
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  MyButton(
+                    child: Text("测试界面"),
+                    onTap: () {
+                      Navigator.of(context).pushNamed('/test');
+                    },
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  MyButton(
+                    child: Text("用户界面"),
+                    onTap: () {
+                      Navigator.of(context).pushNamed('/home');
+                    },
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
